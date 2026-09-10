d9ae6dd21e25419a3d936dc3ff71face3d6f2366
myselfBZ <boburforfun@gmail.com>
2026-09-10 19:14:02 +0500

    renaming migrations

--- cmd/migrate/migrations/002_create_products.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE products(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,
-    price numeric(15, 2) NOT NULL CHECK (price >= 0),
-    stock_quantity INT NOT NULL CHECK (stock_quantity >= 0)
-);
--- cmd/migrate/migrations/004_order_items.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE order_items (
-    order_id UUID NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
-    product_id UUID NOT NULL REFERENCES products(id) ON DELETE RESTRICT,
-    quantity INTEGER NOT NULL CHECK (quantity > 0),
-    unit_price NUMERIC(15, 2) NOT NULL CHECK (unit_price >= 0),
-    
-    PRIMARY KEY (order_id, product_id)
-);
