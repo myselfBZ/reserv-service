f547a5b7864ae2a3b7e749e56120636bcb34ee05
myselfBZ <boburforfun@gmail.com>
2026-09-10 19:11:12 +0500

    clean-up: unnecessary feat users

--- cmd/migrate/migrations/001_create_users.up.sql
+++
@@ -1,9 +0,0 @@
--- CREATE TABLE users(
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     first_name VARCHAR(255) NOT NULL,
---     last_name VARCHAR(255),
---     email VARCHAR(255) NOT NULL UNIQUE,
---     password VARCHAR(255) NOT NULL,
---     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
---     updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
--- );
