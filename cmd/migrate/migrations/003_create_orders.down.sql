799a0328d7f7b5774355fd99e5d5eb0e319308fa
myselfBZ <boburforfun@gmail.com>
2026-09-10 17:43:09 +0500

    migrations v0.0.1

--- cmd/migrate/migrations/003_create_orders.down.sql
+++ cmd/migrate/migrations/003_create_orders.down.sql
@@ -1,1 +1,2 @@
 DROP TABLE IF EXISTS orders;
+DROP TYPE order_status;
--- cmd/migrate/migrations/003_create_orders.up.sql
+++ cmd/migrate/migrations/003_create_orders.up.sql
@@ -2,9 +2,8 @@ CREATE TYPE order_status AS ENUM ('pending', 'confirmed', 'cancelled');
 
 CREATE TABLE orders(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    product_id UUID NOT NULL,
     user_id UUID NOT NULL,
     placed_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    total_price numeric(15, 2) NOT NULL CHECK (total_price >= 0),
     status order_status NOT NULL DEFAULT 'pending'
 );
-
